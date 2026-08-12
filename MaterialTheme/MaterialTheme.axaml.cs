db7ca311aa37ac0cda3628540c781827151f936d
gor1kartem <gorikartemm@gmail.com>
2026-08-12 15:31:37 +0300

    Create sample project and move library to a diffrent folder

---
+++ MaterialTheme/MaterialTheme.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace MaterialTheme;
+
+public class MaterialTheme : Styles
+{
+    public MaterialTheme()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
